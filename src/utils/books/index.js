fbee86930cd90cbd1d0f0749c708c205e57d7be6
Shashwat Sinha <shashwat_sinha@external.mckinsey.com>
2019-02-04 22:55:51 +0530

    Added utility function to return the list of books with corresponding ratings

--- src/utils/books/index.js
+++ src/utils/books/index.js
@@ -8,5 +8,18 @@ const getRatingFromBookId = (id) => {
   const url = `https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${id}`;
   return httpGet(url).then(requestCallResult => requestCallResult.data);
 };
+const getBooksWithRating = () => getBookListWithoutRating().then((allBooks) => {
+  const allBooksCopy = allBooks.books;
+  const allIds = allBooksCopy.map(book => book.id);
+  const allPromises = allIds.map(id => getRatingFromBookId(id));
+  return Promise.all(allPromises)
+    .then(allRatings => allRatings.map(
+      (rating, index) => Object.assign(allBooksCopy[index], rating),
+    ));
+});
 
-module.exports = { getBookListWithoutRating, getRatingFromBookId };
+module.exports = {
+  getBookListWithoutRating,
+  getRatingFromBookId,
+  getBooksWithRating,
+};
--- tests/src/utils/books/index.test.js
+++ tests/src/utils/books/index.test.js
@@ -1,7 +1,10 @@
 /**
  * @jest-environment node
  */
-const { getBookListWithoutRating, getRatingFromBookId } = require('./../../../../src/utils/books/index');
+const {
+  getBookListWithoutRating,
+  getRatingFromBookId, getBooksWithRating,
+} = require('./../../../../src/utils/books/index');
 
 describe('getBookListWithoutRating', () => {
   it('should fetch all the books from the external API', async () => {
@@ -14,5 +17,12 @@ describe('getRatingFromBookId () :', () => {
   it('should fetch rating of a book with a given id', async () => {
     const rating = await getRatingFromBookId(10);
     expect(typeof (rating.rating)).toEqual('number');
+  });
+});
+
+describe('getBooksWithRating', () => {
+  it('should fetch books with their rating', async () => {
+    const booksWithRatings = await getBooksWithRating();
+    expect(booksWithRatings[0].rating).not.toEqual(null);
   });
 });
