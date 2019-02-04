53443a9e15255eaab527167f99891cba027aa478
Shashwat Sinha <shashwat_sinha@external.mckinsey.com>
2019-02-04 22:24:38 +0530

    Added functionality to retrieve Rating of book given it's Id

--- src/utils/books/index.js
+++ src/utils/books/index.js
@@ -4,6 +4,9 @@ const getBookListWithoutRating = () => {
   const url = 'https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/allBooks';
   return httpGet(url).then(requestResult => requestResult.data);
 };
+const getRatingFromBookId = (id) => {
+  const url = `https://5gj1qvkc5h.execute-api.us-east-1.amazonaws.com/dev/findBookById/${id}`;
+  return httpGet(url).then(requestCallResult => requestCallResult.data);
+};
 
-
-module.exports = { getBookListWithoutRating };
+module.exports = { getBookListWithoutRating, getRatingFromBookId };
--- tests/src/utils/books/index.test.js
+++ tests/src/utils/books/index.test.js
@@ -1,11 +1,18 @@
 /**
  * @jest-environment node
  */
-const { getBookListWithoutRating } = require('./../../../../src/utils/books/index');
+const { getBookListWithoutRating, getRatingFromBookId } = require('./../../../../src/utils/books/index');
 
 describe('getBookListWithoutRating', () => {
   it('should fetch all the books from the external API', async () => {
     const allBooks = await getBookListWithoutRating();
     expect(Array.isArray(allBooks.books)).toEqual(true);
+  });
+});
+
+describe('getRatingFromBookId () :', () => {
+  it('should fetch rating of a book with a given id', async () => {
+    const rating = await getRatingFromBookId(10);
+    expect(typeof (rating.rating)).toEqual('number');
   });
 });
